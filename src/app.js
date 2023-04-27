fabec17b9edf2f694887a222253ef63feb580849
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-04-27 14:27:07 -0300

    trabajando en MVC

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
 
+const methodOverride = require('method-override'); // Para poder usar los métodos PUT y DELETE
 const path = require('path'); //Es necesario para que la carpeta views pueda estar adentro de la carpeta src
 
+
 app.use(express.static(path.join(__dirname, '../public')));  // Necesario para los archivos estáticos en el folder /public
+/*** PUT, DELTE,  ***/
+app.use(methodOverride('_method'));//Para crear, eliminar y modificar.. se puede poner cualquier nombre en '_method'
+
+/****EJS *****/
+app.set('view engine', 'ejs'); 
+app.set('views', path.join(__dirname, '/views'));
+
+
 
 const homeRouter = require('./routes/homeRouter.js')
 
 
 app.use("/", homeRouter);
 
-app.listen(3030,()=> console.log('Levantando un servidor con Express'));
-
-app.get('/', (req, res) => res.send('¡Hola Mundo!'));
+app.listen(3030,()=> console.log('Servidor corriendo en http://localhost:3030'));
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,10 +1,16 @@
 const controlador ={
     index: (req, res)=>{ 
-
-        res.send("Holanda")
+        res.render("index")
+    },
+    create: (req, res)=>{ 
+        res.send("Index")
+    },
+    edit: (req, res)=>{ 
+        res.send("Update")
+    },
+    delete: (req, res)=>{ 
+        res.send("Delete")
     }
-
-
 }
 
 module.exports = controlador;
