28a529ca86d934037198a5ac4118174852e51354
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-05-04 13:35:53 -0300

    trabajando con el Crud

--- src/app.js
+++ src/app.js
@@ -8,15 +8,16 @@ const conexion = require('./database/dbConexion')
 const methodOverride = require('method-override'); // Para poder usar los métodos PUT y DELETE
 const path = require('path'); //Es necesario para que la carpeta views pueda estar adentro de la carpeta src
 
-
+/********Middlewares ********/
 app.use(express.static(path.join(__dirname, '../public')));  // Necesario para los archivos estáticos en el folder /public
 /*** PUT, DELTE,  ***/
 app.use(methodOverride('_method'));//Para crear, eliminar y modificar.. se puede poner cualquier nombre en '_method'
-
 /****EJS *****/
 app.set('view engine', 'ejs'); 
 app.set('views', path.join(__dirname, '/views'));
-
+/***Cappturar informacion que viene del body */
+app.use(express.urlencoded({ extended: false })); // Para capturar el body
+app.use(express.json()); // Para capturar el body
 
 
 const homeRouter = require('./routes/homeRouter.js')
--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,12 +1,12 @@
 
 
+const Product = require('../database/models/Product');
 
 
 const controlador ={
     list: async (req,res)=>{
         try {
-            const dbProducto = require('../database/models/Product');
-            let productos = await dbProducto.find();
+            let productos = await Product.find();
             console.log(productos)
             res.json(productos)
             //res.render("./products/productsList")
@@ -14,22 +14,55 @@ const controlador ={
             console.log(e)
         }
     },
-    detail:(req,res)=>{
-        res.render("./products/productDetail")
+    detail: async(req,res)=>{
+        try {
+            let producto = await Product.findOne({_id: req.params.id});
+            res.render("./products/productDetail",{
+                producto:prducto
+            })
+        } catch (error) {
+                res.json({
+                    mesaje:'error al encontrar el id :',
+                    error: error
+                })
+            }
     },
     create: (req, res)=>{ 
+        
         res.render("./products/productCreate")
     },
-    processCreate: (req, res)=>{ 
-        res.send("Creado")
+    processCreate: async (req, res)=>{ 
+        try {
+            await Product.create(req.body)
+            res.redirect('list')
+        } catch (error) {
+            console.log(error)
+        }
     },
     edit: (req, res)=>{ 
         res.render("./products/productEdit")
     },
-    processEdit: (req, res)=>{ 
-        res.send("Producto editado")
+    processEdit: async(req, res)=>{ 
+        try {
+            let productoEditado=await Product.findByIdAndUpdate( req.params.id, req.body, { useFindAndModify: false});//viene del documento oficial de mogoose
+            res.json(productoEditado)
+        } catch (error) {
+            console.log(error)
+        }
+        //res.send("Producto editado")
     },
-    delete: (req, res)=>{ 
+    delete: async(req, res)=>{ 
+        try {
+            let producto =  await Product.findByIdAndDelete({_id: req.params.id})
+            if (producto){
+                res.json({
+                    estado: TreeWalker,
+                    mesaje:'eliminado!'
+                })
+            }
+        } catch (error) {
+            console.log(error)
+        }
         res.send("El Producto fue eliminado")
     }
 }
