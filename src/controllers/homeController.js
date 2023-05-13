cb9e96fc1f1606e518312acc36fb2d5b006ee350
gonzalomravicich <gonzalomravicich@gmail.com>
2023-05-13 15:57:32 -0300

    funcionamiento y estructura del index

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,31 @@
 const controlador ={
-    index: (req, res)=>{ 
-        res.render("./index")
-    }
+    index: async (req,res)=>{
+        const Product = require('../database/models/Products');
+        //const category = require('../database/models/Category');//llamar para hacer la relacion 
+        try {
+            const productos = await Product.find().populate(['category','marca']);
+            
+            const ultimosPoducts = productos.slice(productos.length -6);
+            const destacados = productos.filter(producto=> producto.top_seller == 1)
+            
+            const promedioOfertas = ()=>{                                  //creando una funcion para sacar el promedio de los productos con mejor oferta
+                let productosO = productos.filter(prod=> prod.offer>0)
+                let total= 0;
+                productosO.forEach(prod=>{
+                    total = total + prod.offer
+                })
+                return total/productosO.length;
+            }
+            const ofertas = productos.filter(producto=> producto.offer > promedioOfertas() )
+           
+           res.render("./index",{
+                ultimosPoducts,
+                destacados,
+                ofertas
+                                 })
+        } catch (error) {
+            res.send('error')
+        }
 }
-
+}
 module.exports = controlador;
